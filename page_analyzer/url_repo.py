0f62794b4ff75311d0abb5cda748fb65f70cba64
AssemblerBossss <nazarovmisa39@mail.ru>
2024-10-08 18:56:44 +0300

    получение checka определенного url и лоабвление нового checka

--- page_analyzer/url_repo.py
+++ page_analyzer/url_repo.py
@@ -1,9 +1,9 @@
 from datetime import datetime
+from typing import NamedTuple, NoReturn
 from psycopg2.extras import NamedTupleCursor
 from contextlib import contextmanager
 import os
 import psycopg2
-
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
@@ -22,6 +22,16 @@ def get_db_connection():
 
 
 def get_url_id_if_exists(url):
+    """
+    Проверяет, существует ли заданный URL в базе данных, и возвращает его ID, если он существует.
+
+    Args:
+        url (str): URL для проверки.
+
+    Returns:
+        int or None: ID URL, если он существует, иначе None.
+    """
+
     with get_db_connection() as connection:
         with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
             cursor.execute(
@@ -31,7 +41,14 @@ def get_url_id_if_exists(url):
             return result.id if result else None
 
 
-def get_all_urls():
+def get_all_urls() ->list[NamedTuple]:
+    """
+    Возвращает все URL из базы данных, отсортированные по дате создания в порядке убывания.
+
+    Returns:
+        list: Список всех URL, отсортированных по дате создания в порядке убывания.
+    """
+
     with get_db_connection() as connection:
         with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
             cursor.execute(
@@ -40,23 +57,49 @@ def get_all_urls():
             return cursor.fetchall()
 
 
-def add_url(url) -> int:
+def add_new_url_to_db(url) -> int:
+    """
+    Добавляет новый URL в базу данных и возвращает его ID.
+
+    Args:
+        url (str): URL для добавления.
+
+    Returns:
+        int: ID нового URL.
+    """
+
     current_date = datetime.now().strftime('%Y-%m-%d')
     with get_db_connection() as connection:
         with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
             cursor.execute(
-                '''INSERT INTO urls (name, created_at)
-                 VALUES (%s, %s, %s) RETURNING id''', (url, current_date)
+                'INSERT INTO urls (name, created_at)\n'
+                '                 VALUES (%s, %s, %s) RETURNING id', (url, current_date)
             )
-            new_id = cursor.fetchone()[0]
+            new_id: int = cursor.fetchone()[0]
             connection.commit()
             return new_id
 
 
-def get_url_by_name(connection, name):
-    with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute(
-            '''SELECT * FROM urls 1 WHERE name = %s''', (name,)
-        )
-        return cursor.fetchone()
+def get_url_checks_by_id(url_id: int) -> list[NamedTuple]:
+    with get_db_connection() as connection:
+        with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute(
+                'SELECT * FROM url_checks WHERE url_id = %s\n'
+                '                 ORDER BY created_at DESC, id DESC', (url_id,)
+            )
+            return cursor.fetchall()
+
+
+
+def add_url_check(id, statis_code, h1, title, description) -> NoReturn:
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    with get_db_connection() as connection:
+        with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute(
+                '''INSERT INTO url_checks (url_id, statis_code, h1, title, description, created_at)
+                VALUES (%s, %s, %s, %s, %s, %s)''', (id, statis_code, h1, title, description, current_date)
+            )
+            connection.commit()
+
+
 
